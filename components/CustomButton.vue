bf2ecca7232e98815344e2c14b36138ffbdd032c
arbidemiraj <arbi.demiraj@mail.polimi.it>
2024-05-22 15:37:11 +0200

    navbar

---
+++ components/CustomButton.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="button-container">
+      <button class="btn-primary" @click="handleClick">Get Help</button>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'MyButtonComponent',
+    methods: {
+    handleClick() {
+        this.$router.push('/contact');
+    }
+    }
+  }
+  </script>
+  
+  <style scoped> 
+  .btn-primary {
+    background-color: #DDB98E;
+    color: white;
+    border: none;
+    padding: 10px 15px;
+    font-size: 18px;
+    border-radius: 8px;
+    cursor: pointer;
+    transition: background-color 0.3s;
+  }
+  
+  .btn-primary:hover {
+    opacity: 0.6;
+  }
+  </style>
+  
---
+++ components/Header.vue
@@ -0,0 +1,68 @@
+<template>
+    <header class="site-header">
+        <nav>
+            <NuxtLink to="/"><img class='logo-image' src='/assets/images/logo.png' :width="200"></NuxtLink>
+            <ul>
+                <li>
+                    <NuxtLink to="/aboutUs">About us</NuxtLink>
+                </li>
+                <li>
+                    <NuxtLink to="/services">Activities</NuxtLink>
+                </li>
+                <li>
+                    <NuxtLink to="/team">Team</NuxtLink>
+                </li>
+                <li>
+                    <NuxtLink to="/contact"><CustomButton/></NuxtLink>
+                </li>
+            </ul>
+        </nav>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'Header',
+}
+
+</script>
+
+<style scoped>
+.logo-image:hover {
+    opacity: 0.5;
+}
+
+nav {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    flex: 1;
+    font-size: 20px;
+}
+
+.site-header {
+    height: 55px;
+    background-color: #bb5f75;
+    color: white;
+    padding-bottom: 1rem;
+    padding-inline: 1rem;
+}
+
+.site-header nav ul {
+    list-style: none;
+    justify-content: flex-end;
+    align-items: center;
+    display: flex;
+    gap: 2rem;
+}
+
+.site-header nav ul li a {
+    color: white;
+    text-decoration: none;
+}
+
+.site-header nav ul li a:hover {
+    background-color: #bb5f75;
+    opacity: 0.5;
+}
+</style>
