1b0a4ffa1878b45f031e96a4c68f7fb4be30b7fc
arbidemiraj <arbi.demiraj@mail.polimi.it>
2024-05-23 12:12:22 +0200

    team page

---
+++ components/TeamMemberCard.vue
@@ -0,0 +1,59 @@
+<template>
+  <NuxtLink class="team-member" to="/team">
+      <img :src="image" :alt="name" class="team-member__image" />
+      <h3 class="team-member__name">{{ name }}</h3>
+      <p class="team-member__role">{{ role }}</p>
+    </NuxtLink>
+  </template>
+
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      required: true
+    },
+    role: {
+      type: String,
+      required: true
+    },
+    image: {
+      type: String,
+      required: true
+    }
+  }
+};
+</script>
+
+<style scoped>
+.team-member {
+    background-color: rgb(249, 247, 247);
+    border-radius: 20px;
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+    overflow: hidden;
+    width: 100%;
+    max-width: calc(20% - 50px); /* Ensures two cards per row with a gap */
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    text-align: center;
+    margin: 1rem;
+    text-decoration: none;
+}
+.team-member:hover {
+    box-shadow: 0 8px 16px rgba(0, 0, 0, 0.1);
+    transform: translateY(-7px);
+    transition: all 0.3s;
+}
+.team-member__image {
+  border-radius: 50%;
+  width: 100px;
+  height: 100px;
+}
+.team-member__name {
+  margin: 0.5rem 0;
+}
+.team-member__role {
+  color: gray;
+}
+</style>
---
+++ pages/team.vue
@@ -0,0 +1,63 @@
+<template>
+    <div class="team">
+        <div class="title-background">
+            <h1 class=title>Our<br>Team</h1>
+        </div>
+      <div class="team__members">
+        <TeamMemberCard
+          v-for="(member, index) in teamMembers"
+          :key="index"
+          :name="member.name"
+          :role="member.role"
+          :image="member.image"
+        />
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: "Team",
+    data() {
+      return {
+        teamMembers: [
+          { name: "Ethan Harris", role: "Role", image: "./assets/images/ethan-harris.png" },
+          { name: "Olivia Parker", role: "Role", image: "/assets/images/olivia-parker.png" },
+          { name: "Ethan Harris", role: "Role", image: "/assets/images/ethan-harris.png" },
+          { name: "Olivia Parker", role: "Role", image: "/assets/images/olivia-parker.png" },
+          { name: "Ethan Harris", role: "Role", image: "/assets/images/ethan-harris.png" },
+          { name: "Olivia Parker", role: "Role", image: "/assets/images/olivia-parker.png" },
+          { name: "Ethan Harris", role: "Role", image: "/assets/images/ethan-harris.png" },
+          { name: "Olivia Parker", role: "Role", image: "/assets/images/olivia-parker.png" }
+        ]
+      };
+    }
+  };
+  </script>
+  
+  <style scoped>
+  .team {
+    text-align: center;
+  }
+  .team__header {
+    margin-bottom: 2rem;
+  }
+  .title-background {
+        background-image: url('assets/images/image.png');
+        background-size: cover; 
+        background-position: center;
+        padding: 5px;
+        text-align: left;
+        color: white;
+        width: 100%;
+        box-shadow: 5px black;
+    }
+  .team__members {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    margin: 30px;
+    gap: 2rem;
+  }
+  </style>
+  
