7142832ba3fcdbfe96129abe8971cad325145521
arbidemiraj <arbi.demiraj@mail.polimi.it>
2024-05-23 15:47:37 +0200

    db fix 2

---
+++ pages/login.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="login">
+      <h1>Login Page</h1>
+      <!-- Add your login form here -->
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'Login'
+  };
+  </script>
+  
+  <style scoped>
+  /* Add your styles here */
+  </style>
+  
